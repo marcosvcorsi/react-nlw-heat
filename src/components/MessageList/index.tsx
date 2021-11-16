ac339642985fbf4b495d98de28e9430eda1a8184
Marcos Corsi <marcosvcorsi@gmail.com>
2021-11-15 23:17:04 -0300

    feat: add message list api integration

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,57 +1,47 @@
 import styles from './styles.module.scss';
 
 import logoImage from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('/messages/last').then(({ data }) => {
+      setMessages(data);
+    })
+  }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImage} alt="DoWhile2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            evento do ano.
-          </p>
+        {messages.map(message => (
+          <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>
+              {message.text}
+            </p>
 
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/marcosvcorsi.png" alt="Marcos Vinicius Corsi" />
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt={message.user.name} />
+              </div>
+
+              <span>{message.user.name}</span>
             </div>
-
-            <span>Marcos Vinicius Corsi</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            evento do ano.
-          </p>
-
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/marcosvcorsi.png" alt="Marcos Vinicius Corsi" />
-            </div>
-
-            <span>Marcos Vinicius Corsi</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            evento do ano.
-          </p>
-
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/marcosvcorsi.png" alt="Marcos Vinicius Corsi" />
-            </div>
-
-            <span>Marcos Vinicius Corsi</span>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   )
